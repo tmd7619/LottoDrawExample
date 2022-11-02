4235a075b1957831bc5635b90453c2de3bf38167
tmd7619 <tmd814@naver.com>
2022-11-02 14:01:00 +0900

    Add Draw LottoNum

--- LottoDrawExample/ViewController.swift
+++ LottoDrawExample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // tableView Outlet 생성
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,7 +26,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // 해당 섹션의 셀 개수 - 필수 메서드
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return lottoNumbers.count
     }
     
     // 셀 생성과 반환 - 필수 메서드
@@ -31,17 +34,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "lottoCell", for: indexPath as IndexPath) as! LottoCell
         
-        cell.number1.text = "1"
-        cell.number2.text = "2"
-        cell.number3.text = "3"
-        cell.number4.text = "4"
-        cell.number5.text = "5"
-        cell.number6.text = "6"
-
+        let row: Int = indexPath.row
         
+        cell.number1.text = "\(lottoNumbers[row][0])"
+        cell.number2.text = "\(lottoNumbers[row][1])"
+        cell.number3.text = "\(lottoNumbers[row][2])"
+        cell.number4.text = "\(lottoNumbers[row][3])"
+        cell.number5.text = "\(lottoNumbers[row][4])"
+        cell.number6.text = "\(lottoNumbers[row][5])"
         return cell
     }
     
-
+    
+    
+    // 이중배열 변수 추가
+    var lottoNumbers = Array<Array<Int>>()
+    
+    // Draw action button
+    @IBAction func doDraw(_ sender: UIBarButtonItem) {
+        lottoNumbers = Array<Array<Int>>()
+        
+        var originalNumbers = Array(1 ... 45)
+        var index = 0
+        
+        // 생성할 로또 번호 셀은 총 5개 // 총 5회의 로또 번호 생성 과정 반복
+        for _ in 0 ... 4 {
+            originalNumbers = Array(1 ... 45)
+            var columnArray = Array<Int>()
+            
+            // 한 과정당 6개의 번호를 생성하기 위해 중첩 for 문 사용
+            for _ in 0 ... 5 {
+                // 랜덤번호 생성
+                index = Int(arc4random_uniform(UInt32(originalNumbers.count)))
+                print("for 문 안에 originalNumbers[index]) : \(originalNumbers[index])")
+                columnArray.append(originalNumbers[index])
+            }
+            // 추출한 번호들을 작은 숫자대로 배열 정렬 // sort 메서드 사용
+            columnArray.sort(by: { $0 < $1 })
+            lottoNumbers.append(columnArray)
+            
+        }
+        // reloadData 메서드 호출
+        tableView.reloadData()
+    }
 }
 
